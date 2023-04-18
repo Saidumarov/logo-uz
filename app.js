b90b7f025eb85f48cd9122e0e043161d8b98c1e7
Saidumarov <saidumarovjafarxon06@gmail.com>
2023-04-18 16:23:12 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,170 @@
+let b = document.querySelector("body")
+
+function ok1() {
+
+  if (b.style.backgroundColor = "white") {
+    document.getElementById("l").style.color = "black"
+    b.style.color = "black"
+    document.querySelector(".b1").style.backgroundColor = String(" rgba(243, 243, 243, 0.5)")
+    document.querySelector(".b2").style.backgroundColor = String(" rgba(243, 243, 243, 0.5)")
+    document.querySelector(".b3").style.backgroundColor = String(" rgba(243, 243, 243, 0.5)")
+    document.querySelector(".b4").style.backgroundColor = String(" rgba(243, 243, 243, 0.5)")
+    document.querySelector(".b5").style.backgroundColor = String(" rgba(243, 243, 243, 0.5)")
+    document.querySelector(".b6").style.backgroundColor = String(" rgba(243, 243, 243, 0.5)")
+    document.querySelector("body").style.backgroundimage = "url('./ac.png')";
+    document.querySelector(".mom").style.backgroundColor = String(" rgba(23, 25, 29, 0.1) ")
+    document.querySelector(".oy").style.backgroundColor = String("blue")
+    document.querySelector(".oy").style.left = ("0%")
+    document.querySelector(".oy").style.transition = ("0.9s")
+    document.getElementById("nav").style.backgroundColor = String(" rgba(23, 25, 29, 0.1) ")
+    document.querySelector(".menyu").style.backgroundColor = String("rgba(255, 255, 255, 0.5)")
+    document.querySelector(".rang").style.color = "black"
+    document.querySelector(".rang1").style.color = "black"
+    document.querySelector(".rang2").style.color = "black"
+    document.querySelector(".rang3").style.color = "black"
+    document.querySelector(".rang4").style.color = "black"
+    document.querySelector(".rang5").style.color = "black"
+    document.querySelector(".rang6").style.color = "black"
+    document.querySelector(".rang7").style.color = "black"
+    document.querySelector(".rang8").style.color = "black"
+    document.querySelector(".rang9").style.color = "black"
+    document.querySelector(".rang10").style.color = "black"
+    document.querySelector(".rang11").style.color = "black"
+    document.querySelector(".rang12").style.color = "black"
+    document.querySelector(".rang13").style.color = "black"
+    document.querySelector(".rang14").style.color = "black"
+    document.querySelector(".rang15").style.color = "black"
+    document.querySelector(".rang16").style.color = "black"
+    document.querySelector(".rang17").style.color = "black"
+    document.querySelector(".rang18").style.color = "black"
+    document.querySelector(".rang19").style.color = "black"
+    document.querySelector(".rang20").style.color = "black"
+    document.querySelector(".rang21").style.color = "black"
+    document.querySelector(".rang22").style.color = "black"
+    document.querySelector(".rang23").style.color = "black"
+    document.querySelector(".rang24").style.color = "black"
+    document.querySelector(".rang25").style.color = "black"
+    document.querySelector(".rang26").style.color = "black"
+    document.querySelector(".rang27").style.color = "black"
+    document.querySelector(".rang28").style.color = "black"
+
+  }
+
+}
+
+
+function ok2() {
+
+  if (b.style.backgroundColor = "#111317") {
+    document.getElementById("l").style.color = "white"
+    b.style.color = "white"
+    document.querySelector(".b1").style.backgroundColor = String("rgba(17, 19, 23, 0.5)")
+    document.querySelector(".b2").style.backgroundColor = String("rgba(17, 19, 23, 0.5)")
+    document.querySelector(".b3").style.backgroundColor = String("rgba(17, 19, 23, 0.5)")
+    document.querySelector(".b4").style.backgroundColor = String("rgba(17, 19, 23, 0.5)")
+    document.querySelector(".b5").style.backgroundColor = String("rgba(17, 19, 23, 0.5)")
+    document.querySelector(".b6").style.backgroundColor = String("rgba(17, 19, 23, 0.5)")
+    document.querySelector(".mom").style.backgroundColor = String(" rgba(23, 25, 29, 0.1) ")
+    document.querySelector(".b1").style.borderColor = String("#4E4E4E")
+    document.querySelector(".b2").style.borderColor = String("#4E4E4E")
+    document.querySelector(".b3").style.borderColor = String("#4E4E4E")
+    document.querySelector(".b4").style.borderColor = String("#4E4E4E")
+    document.querySelector(".b5").style.borderColor = String("#4E4E4E")
+    document.querySelector(".b6").style.borderColor = String("#4E4E4E")
+    document.querySelector("body").style.backgroundimage = " url('./ac.png')";
+    document.querySelector(".oy").style.left = ("57%")
+    document.querySelector(".oy").style.backgroundColor = String("blue")
+    document.querySelector(".oy").style.transition = ("0.9s")
+    document.querySelector(".oy").style.opacity = ("10")
+    document.getElementById("nav").style.backgroundColor = String("rgba(23, 25, 29, 0.7)")
+    document.querySelector(".menyu").style.backgroundColor = String(" rgba(17, 19, 23, 0.5)")
+    document.querySelector(".rang").style.color = "white"
+    document.querySelector(".rang1").style.color = "white"
+    document.querySelector(".rang2").style.color = "white"
+    document.querySelector(".rang3").style.color = "white"
+    document.querySelector(".rang4").style.color = "white"
+    document.querySelector(".rang5").style.color = "white"
+    document.querySelector(".rang6").style.color = "white"
+    document.querySelector(".rang7").style.color = "white"
+    document.querySelector(".rang8").style.color = "white"
+    document.querySelector(".rang9").style.color = "white"
+    document.querySelector(".rang10").style.color = "white"
+    document.querySelector(".rang11").style.color = "white"
+    document.querySelector(".rang12").style.color = "white"
+    document.querySelector(".rang13").style.color = "white"
+    document.querySelector(".rang14").style.color = "white"
+    document.querySelector(".rang15").style.color = "white"
+    document.querySelector(".rang16").style.color = "white"
+    document.querySelector(".rang17").style.color = "white"
+    document.querySelector(".rang18").style.color = "white"
+    document.querySelector(".rang19").style.color = "white"
+    document.querySelector(".rang20").style.color = "white"
+    document.querySelector(".rang21").style.color = "white"
+    document.querySelector(".rang22").style.color = "white"
+    document.querySelector(".rang23").style.color = "white"
+    document.querySelector(".rang24").style.color = "white"
+    document.querySelector(".rang25").style.color = "white"
+    document.querySelector(".rang26").style.color = "white"
+    document.querySelector(".rang27").style.color = "white"
+    document.querySelector(".rang28").style.color = "white"
+  }
+
+}
+
+
+function m1() {
+
+  document.querySelector(".yumaloq").style.left = ("0%")
+  document.querySelector(".rang21").style.color = ("white")
+  // document.querySelector(".rang22").style.color = ("black")
+  // document.querySelector(".rang23").style.color = ("black")
+  // document.querySelector(".rang24").style.color = ("black")
+  // document.querySelector(".rang25").style.color = ("black")
+}
+
+function m2() {
+
+if (  document.querySelector(".yumaloq").style.left = ("20%")) {
+  // document.querySelector(".rang21").style.color = ("black")
+  document.querySelector(".rang22").style.color = ("white")
+  // document.querySelector(".rang23").style.color = ("black")
+  // document.querySelector(".rang24").style.color = ("black")
+  // document.querySelector(".rang25").style.color = ("black")
+} 
+
+}
+
+
+function m3() {
+  document.querySelector(".yumaloq").style.left = ("40%")
+  // document.querySelector(".rang21").style.color = ("black")
+  // document.querySelector(".rang22").style.color = ("black")
+  document.querySelector(".rang23").style.color = ("white")
+  // document.querySelector(".rang24").style.color = ("black")
+  // document.querySelector(".rang25").style.color = ("black")
+}
+
+
+function m4() {
+  document.querySelector(".yumaloq").style.left = ("60%")
+  // document.querySelector(".rang21").style.color = ("black")
+  // document.querySelector(".rang22").style.color = ("black")
+  // document.querySelector(".rang23").style.color = ("black")
+  document.querySelector(".rang24").style.color = ("white")
+  // document.querySelector(".rang25").style.color = ("black")
+}
+
+function m5() {
+  document.querySelector(".yumaloq").style.left = ("77.1%")
+  // document.querySelector(".rang21").style.color = ("black")
+  // document.querySelector(".rang22").style.color = ("black")
+  // document.querySelector(".rang23").style.color = ("black")
+  // document.querySelector(".rang24").style.color = ("black")
+if (  document.querySelector(".rang25").style.color = ("white")) {
+
+}
+}
+
+
+
+
